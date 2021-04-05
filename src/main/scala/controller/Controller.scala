06af6d18b05896c4ab44c45b04142d6282187c59
Sarah <sarah.tiefert@htwg-konstanz.de>
2021-04-05 21:57:39 +0200

    defined Controller Class for MVC Architecture

--- src/main/scala/controller/Controller.scala
+++ src/main/scala/controller/Controller.scala
@@ -1,6 +1,28 @@
 package controller
-import module.Table
+import module.{Deck, Table, TableGenerator}
+import util.Observable
 
-class Controller(var table:Table){
+class Controller(var table:Table) extends Observable{
+  def createTestTable(noOfPlayers:Int, deck:Deck): Unit = {
+    val tb = TableGenerator(noOfPlayers, 40)
+    table = tb.createTable
+  }
+
+  def tableToString: String = table.toString
+
+  def placeCard(card:Int, place:Int): Unit ={
+    table = table.playerPlacesCard(card, place)
+    notifyObservers()
+  }
+
+  def doubt():Unit = {
+    table = table.playerDoubtsCards
+    notifyObservers()
+  }
+
+  def confirmWinner: Boolean = {
+    notifyObservers()
+    table.playerWon
+  }
 
 }
