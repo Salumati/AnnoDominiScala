55a552f6f1b16fdf836160d46269d00bdfab2734
Sarah <sarah.tiefert@htwg-konstanz.de>
2021-04-10 10:07:05 +0200

    corrected Error in Card Class

--- src/main/scala/module/Card.scala
+++ src/main/scala/module/Card.scala
@@ -4,6 +4,5 @@ case class Card(text: String, year: Int) {
   def showYear:String = year.toString
   def reveal:String = "year: " + showYear + " " +  this.toString
 
-  override def toString: String = "[" + text + "]\n" +
-    ","
+  override def toString: String = "[" + text + "]\n"
 }
