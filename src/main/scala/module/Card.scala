acbca12dc6b5a13f7f07d959f9c908a57a70537d
Sarah <sarah.tiefert@htwg-konstanz.de>
2021-04-10 11:32:31 +0200

    expanded PlayerSpec added some TODOs

--- src/main/scala/module/Card.scala
+++ src/main/scala/module/Card.scala
@@ -1,6 +1,7 @@
 package module
 
 case class Card(text: String, year: Int) {
+  // TODO: get coverage to 100%
   def showYear:String = year.toString
   def reveal:String = "year: " + showYear + " " +  this.toString
 
--- src/main/scala/module/Deck.scala
+++ src/main/scala/module/Deck.scala
@@ -2,6 +2,7 @@ package module
 import scala.util.Random
 
 case class Deck(cards:List[Card]=Nil) {
+  // TODO: get coverage to 100%
   def drawCard: (Card, List[Card]) = (cards.head, cards.tail)
   def drawCard(n: Int) : (List[Card], List[Card]) = cards.splitAt(n)
 
